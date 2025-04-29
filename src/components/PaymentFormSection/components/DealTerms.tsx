ee0d53202fd83bdf241005fc4f6cf55955192a31
Dan Dudaiev <122373927+UiMi17@users.noreply.github.com>
2025-04-29 18:48:35 +0300

    ADDED: DealTerms component for PaymentFormSection.

---
+++ src/components/PaymentFormSection/components/DealTerms.tsx
@@ -0,0 +1,11 @@
+const DealTerms = () => {
+    return <div className='mt-2 py-3 px-4 border border-border-primary rounded-lg'>
+        <p className='text-[12px] leading-[calc(16/12) text-secondary'>You'll have your <span
+            className='font-semibold text-[12px] leading-[calc(16/12)]'>Plan Pro during 1 year. </span> After this
+            period
+            of time, your plan will be <span className='font-semibold text-[12px] leading-[calc(16/12)]'>automatically renewed&nbsp;</span>
+            with its original price without any discounts applied.</p>
+    </div>
+}
+
+export default DealTerms;
--- src/components/PaymentFormSection/index.tsx
+++ src/components/PaymentFormSection/index.tsx
@@ -1,8 +1,10 @@
 import CardPaymentForm from "./components/CardPaymentForm.tsx";
+import DealTerms from "./components/DealTerms.tsx";
 
 const PaymentFormSection = () => {
     return <section className='pt-4'>
         <CardPaymentForm/>
+        <DealTerms/>
     </section>
 }
 
